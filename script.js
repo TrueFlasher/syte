992b77e4a16e60640335889ff09ca4d58d1d3d79
TrueFlasher <91925260+TrueFlasher@users.noreply.github.com>
2021-12-26 20:07:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
+let nav = document.querySelector('.navigation');
+
 document.querySelectorAll('.burger').forEach(item =>{
     item.addEventListener('click', function(){
-        document.querySelector('.navigation').classList.toggle('active');
+       nav.classList.toggle('active');
     })
 })
-
-let buttons = document.querySelectorAll('.nav-item');
-let blocks = document.querySelectorAll('.scroll');
-
-buttons.forEach((button, index) => {
-    button.addEventListener('click', function(){
-        blocks[index].scrollIntoView(
-            {
-                block: 'start',
-                behavior: 'smooth',
-            }
-        );
-    })
-})
-
-let nav = document.querySelector('.navigation');
-let body = document.body;
 
 window.addEventListener('scroll', function(){
     if (window.pageYOffset < 100){
@@ -28,4 +13,3 @@ window.addEventListener('scroll', function(){
         nav.classList.add('fixed');
     }
 })
-
